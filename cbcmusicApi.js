2e20817d432b6d40563caf3ccaf9cf5c475c0a86
TANDRA CHAKRABORTY <TANDRA.CHAKRABORTY@cbc.ca>
2019-02-02 11:57:52 -0500

    typo fixed with new file

---
+++ cbcmusicApi.js
@@ -0,0 +1,19 @@
+const request = require('request-promise-native')
+const routes = require('./routes')
+
+
+const getLive = langPrefix => {
+  return request({
+    uri: `${routes.apiClient}/api/v1/${langPrefix}/music/live`,
+    json: true
+  })
+}
+
+
+module.exports = {
+  getLive
+}
+
+
+
+

b33f74397544ffcfae97c6456b01cd2a76c9e590
TANDRA CHAKRABORTY <TANDRA.CHAKRABORTY@cbc.ca>
2019-02-01 20:22:35 -0500

    testing first commit

---
+++ routes.js
@@ -0,0 +1,6 @@
+const routes = {
+  nlu: '',
+  apiClient: ''
+}
+
+module.exports = routes
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const request = require('request-promise-native')
+const routes = require('./routes')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.get('/', (req, res) => res.send('This is moonshot master api'))
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
